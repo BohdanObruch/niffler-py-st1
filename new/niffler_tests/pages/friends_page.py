eb2929c27cc0259132a273a2cedf4f620137f7c0
Bohdan Obruch <bodan1992@gmail.com>
2024-08-23 22:37:26 +0300

    Moved niffler app to a separate folder

--- new/niffler_tests/pages/friends_page.py
+++
@@ -1,9 +0,0 @@
-from niffler_tests.pages.base_page import BasePage
-
-
-class FriendsPage(BasePage):
-    def __init__(self):
-        super().__init__()
-
-
-friends = FriendsPage()
--- new/niffler_tests/utils/random_data.py
+++
@@ -1,9 +0,0 @@
-from faker import Faker
-
-
-def generate_random_data():
-    fake = Faker()
-    username = fake.user_name()
-    password = fake.password()
-
-    return username, password
